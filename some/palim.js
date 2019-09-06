9f05dc35c17a5d9f8dad810cd2e607224e535b9b
IgnacioCastro <ignaciomenchaca666@gmail.com>
2019-09-06 13:59:28 -0500

    all changes

---
+++ some/palim.js
@@ -0,0 +1,26 @@
+function quitSpaces(str) {
+  let result = '';
+  let arrayA = str.split(' ');
+
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== "")
+      result += arrayA[i];
+  }
+  return result;
+}
+
+function palindrome(string) {
+
+  let str = quitSpaces(string);
+
+  let len = str.length;
+
+  for (let i = 0; i < len/2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
+}
+
+console.log(palindrome('anita lava la tina'));
