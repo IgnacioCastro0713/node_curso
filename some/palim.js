080096eb60bc3839579b4d061ccf665efa9618e7
IgnacioCastro <ignaciomenchaca666@gmail.com>
2019-09-06 14:46:30 -0500

    formatter

--- some/palim.js
+++ some/palim.js
@@ -1,29 +1,24 @@
 function quitSpaces(str) {
-    let result = '';
-    let arrayA = str.split(' ');
+  let result = "";
+  let arrayA = str.split(" ");
 
-    for (let i = 0; i < arrayA.length; i++) {
-        if (arrayA[i] !== "")
-            result += arrayA[i];
-    }
-    return result;
+  for (let i = 0; i < arrayA.length; i++) {
+    if (arrayA[i] !== "") result += arrayA[i];
+  }
+  return result;
 }
-
-
-
 
 function palindrome(string) {
+  let str = quitSpaces(string);
 
-    let str = quitSpaces(string);
+  let len = str.length;
 
-    let len = str.length;
-
-    for (let i = 0; i < len / 2; i++) {
-        if (str[i] !== str[len - 1 - i]) {
-            return false;
-        }
+  for (let i = 0; i < len / 2; i++) {
+    if (str[i] !== str[len - 1 - i]) {
+      return false;
     }
-    return true;
+  }
+  return true;
 }
 
-console.log(palindrome('anita lava la tina'));
+console.log(palindrome("anita lava la tina"));
